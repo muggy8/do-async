5e70c52961d998c3c85120d1ddf8167ab68cc29a
muggy8 <muggy8@gmail.com>
2017-01-11 22:38:31 -0800

    setting up compatibility with node and web js

---
+++ do-async.js
@@ -0,0 +1,9 @@
+(function(context){
+	
+	context.doAsync = function(callback){
+		
+	}
+	
+})(this)
+
+if (typeof module != 'undefined' && module.exports) {module.exports = doAsync;} // export if we're in node
